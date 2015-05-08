176c98dd7fec26980591a9ba3bb71bee1eeab8a7
Arlan Jaska <ajaska@berkeley.edu>
2015-05-08 13:42:11 -0700

    Change singletons to instantiate locks per-class
    Before it was creating one for any singleton object, resulting in
    Bad Things (infinite lock) happening when one singleton object
    instantiates another singleton object in its instantiation. (I hope
    that made sense...)

--- backend/fureon/components/mixins.py
+++ backend/fureon/components/mixins.py
@@ -2,11 +2,14 @@ import threading
 
 
 class SingletonMixin(object):
-    __singleton_lock = threading.Lock()
+    __singleton_lock = None
     __singleton_instance = None
 
     @classmethod
     def instance(cls, *args, **kwargs):
+        if not cls.__singleton_lock:
+            cls.__singleton_lock = threading.Lock()
+
         if not cls.__singleton_instance:
             with cls.__singleton_lock:
                 if not cls.__singleton_instance:
